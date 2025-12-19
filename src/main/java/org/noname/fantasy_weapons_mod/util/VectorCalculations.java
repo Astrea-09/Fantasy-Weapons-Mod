4ea04f58cc98c2ca5063f97ccd403bf017926957
Ill-Come-Up-With-The-Name-Later <adtmax5@gmail.com>
2025-12-19 11:12:57 -0500

    Clarified parameter names of behind method

--- src/main/java/org/noname/fantasy_weapons_mod/util/VectorCalculations.java
+++ src/main/java/org/noname/fantasy_weapons_mod/util/VectorCalculations.java
@@ -6,20 +6,20 @@ public class VectorCalculations {
 
     /**
      * Uses the dot product to determine if vector
-     * a is behind vector b. Vector a is considered
-     * behind vector b if the dot product of a and b is
+     * lineAToB is behind vector bDirection. Vector lineAToB is considered
+     * behind vector bDirection if the dot product of lineAToB and bDirection is
      * greater than or equal to 0.8. A dot product of 1 means the two
      * vectors are facing the exact same direction, so we give some margin.
      *
      * When you use this method, pass in normalized direction vectors.
      *
-     * @param a A vector. Use the normalized form of a vector between
+     * @param lineAToB A vector. Use the normalized form of lineAToB vector between
      *         two points. The vector from V1 to V2 is gotten from V2 - V1.
-     * @param b A vector. Use the normalized forward direction of
+     * @param bDirection A vector. Use the normalized forward direction of
      *         your destination point.
-     * @return If Vector a is behind vector b
+     * @return If Vector lineAToB is behind vector bDirection
      */
-    public static boolean behind(Vec3 a, Vec3 b) {
-        return a.dot(b) >= 0.8;
+    public static boolean behind(Vec3 lineAToB, Vec3 bDirection) {
+        return lineAToB.dot(bDirection) >= 0.8;
     }
 }
