24d610631f4acf8486a9bb34ec970103d0ec264b
Angelica <paganangelica21@gmail.com>
2025-12-19 14:13:20 -0500

    Started adding the longbow and started replacing some of the placeholder pictures with the real textures.

---
+++ src/main/java/org/noname/fantasy_weapons_mod/item/custom/Longbow.java
@@ -0,0 +1,10 @@
+package org.noname.fantasy_weapons_mod.item.custom;
+
+import net.minecraft.world.item.BowItem;
+
+public class Longbow extends BowItem {
+
+    public Longbow(Properties settings) {
+        super(settings);
+    }
+}
